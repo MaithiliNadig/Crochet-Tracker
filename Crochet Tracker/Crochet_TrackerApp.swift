207ce7194b1338d5dc61421cff987f17e4c4c500
Maithili Nadig <maithilinadig@gmail.com>
2026-06-14 14:51:04 -0700

    Initial Commit

---
+++ Crochet Tracker/Crochet_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crochet_TrackerApp.swift
+//  Crochet Tracker
+//
+//  Created by Maithili Nadig on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Crochet_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Crochet TrackerTests/Crochet_TrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Crochet_TrackerTests.swift
+//  Crochet TrackerTests
+//
+//  Created by Maithili Nadig on 6/14/26.
+//
+
+import Testing
+@testable import Crochet_Tracker
+
+struct Crochet_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
